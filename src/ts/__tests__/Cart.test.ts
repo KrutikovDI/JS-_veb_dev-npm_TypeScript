6d2f141c678cabb0f8909b645bf8195ce30cf33d
Danil Krutikov <kdanil.i@yandex.ru>
2024-02-29 19:49:00 +0700

    Second commit

--- src/ts/__tests__/Cart.test.ts
+++ src/ts/__tests__/Cart.test.ts
@@ -2,6 +2,38 @@ import Cart from '../service/Cart';
 
 test('new card should be empty', () => {
   const cart = new Cart();
-
-  expect(cart.items.length).toBe(0);
-});
+  expect(cart.items.length).toEqual(0);
+})
+ 
+test('Добавим объекты в массив', () => {
+  const cart = new Cart();
+  cart.add({id: 1, name: 'Apple', price: 100});
+  cart.add({id: 2, name: 'Banana', price: 200});
+  cart.add({id: 3, name: 'Mango', price: 300});
+  const correct = {id: 2, name: 'Banana', price: 200};
+  expect(cart.items[1]).toEqual(correct);
+})
+ 
+test('Найдем сумму всех товаров', () => {
+  const cart = new Cart();
+  cart.add({id: 1, name: 'Apple', price: 100});
+  cart.add({id: 2, name: 'Banana', price: 200});
+  cart.add({id: 3, name: 'Mango', price: 300});
+  expect(cart.sum()).toBe(600); 
+})
+ 
+test ('Найдем сумму всех товаров со скидкой', ()=> {
+  const cart = new Cart();
+  cart.add({id: 1, name: 'Apple', price: 100});
+  cart.add({id: 2, name: 'Banana', price: 200});
+  cart.add({id: 3, name: 'Mango', price: 300});
+  expect (cart.sumWithDiscount(20)).toBe(540);
+})
+ 
+test ('Удалим товар из списка', () => {
+  const cart = new Cart();
+  cart.add({id: 1, name: 'Apple', price: 100});
+  cart.add({id: 2, name: 'Banana', price: 200});
+  cart.add({id: 3, name: 'Mango', price: 300});
+  expect(cart.removeProduct(2)).toBe(2);
+})
--- src/ts/service/Cart.ts
+++ src/ts/service/Cart.ts
@@ -1,20 +1,28 @@
 import Buyable from '../domain/Buyable';
-
+ 
 export default class Cart {
-    private _items: Buyable[] = [];
-
-    add(item: Buyable): void {
-        this._items.push(item);
-    }
-
-    get items(): Buyable[] {
-        return [...this._items]; 
-    }
-
-    sum(): Buyable[] {
-      let sum: number;
-      this._items.forEach(i => sum += i.price)
-      return sum
-      // return this._items.reduce((sum: number, prod: Buyable[]) => {sum + prod.price}, 0);
-    }
+  private _items: Buyable[] = [];
+ 
+  add(item: Buyable): void {
+      this._items.push(item);
+  }
+ 
+  get items(): Buyable[] {
+      return [...this._items];
+  }
+ 
+  sum(): number {
+    return this._items.reduce((sum: number, prod: Buyable) => sum + prod.price, 0);
+  }
+ 
+  sumWithDiscount(discount: number): number {
+    return this._items.reduce((sum: number, prod: Buyable) => sum + (prod.price - discount), 0);
+  }
+ 
+  removeProduct(id: number): void {
+    let index: number = this._items.findIndex(i => i.id === id)
+    if (index == -1) {
+      console.log("Элемент с таким id не найден")
+    } else {this._items.splice(index, 1)}
+  }
 }
